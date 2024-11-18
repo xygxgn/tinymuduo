1a5b0815935c6233c89f63b594093eb0862b94b6
xygxgn <hcxygxgn@163.com>
2024-11-17 23:58:05 -0800

    first commit

--- include/Logger.hh
+++ include/Logger.hh
@@ -1,2 +1,26 @@
 #pragma once
 
+#include <string>
+
+#include "noncopyable.hh"
+
+enum LogLevel
+{
+    INFO,
+    WARN,
+    ERROR,
+    FATAL,
+    DEBUG
+};
+
+class Logger : noncopyable
+{
+public:
+    static Logger& instance();
+    void setLogLevel(int level);
+    void log(std::string& msg);
+private:
+    Logger() = default;
+    int logLevel_;
+};
+
---
+++ src/Logger.cc
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+
+#include "Logger.hh"
+
+Logger& Logger::instance()
+{
+    static Logger logger;
+    return logger;
+}
+
+void Logger::setLogLevel(int level)
+{
+    logLevel_ = level;
+}
+
+void Logger::log(std::string& msg)
+{
+    switch (logLevel_)
+    {
+    case INFO:
+        std::cout << "[INFO]";
+        break;
+    case WARN:
+        std::cout << "[WARN]";
+        break;
+    case ERROR:
+        std::cout << "[ERROR]";
+        break;
+    case FATAL:
+        std::cout << "[FATAL]";
+        break;
+    case DEBUG:
+        std::cout << "[DEBUG]";
+        break;
+    default:
+        break;
+    }
+}
