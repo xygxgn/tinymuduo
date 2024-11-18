c2b73b047477303bae38bc46e48957651bb624e3
xygxgn <hcxygxgn@163.com>
2024-11-18 01:57:20 -0800

    first commit

--- include/Channel.hh
+++ include/Channel.hh
@@ -1,17 +1,42 @@
 #ifndef _TINYMUDUO_CHANNEL_H
 #define _TINYMUDUO_CHANNEL_H
 
-class Channel
+#include "noncopyable.hh"
+#include "Timestamp.hh"
+
+#include <functional>
+#include <memory>
+
+class EventLoop;
+
+class Channel : noncopyable
 {
 public:
+    using EventCallback = std::function<void()>;
+    using ReadEventCallback = std::function<void(Timestamp)>;
+
     Channel();
-    ~Channel();
+    Channel(EventLoop *loop, int fd);
+    void handleEvent(Timestamp receieveTime);
+
 private:
-    
+    static const int kNodeEvent;
+    static const int kReadEvent;
+    static const int kWriteEvent;
+
+    EventLoop *loop_;
+    const int *fd_;
+    int events_;
+    int revents_;
+    int index_;
+
+    std::weak_ptr<void> tie_;
+    bool tied_;
+
+    ReadEventCallback readCallback_;
+    EventCallback writeCallback_;
+    EventCallback closeCallback_;
+    EventCallback errorCallback_;
 };
-
-
-
-
 
 #endif
