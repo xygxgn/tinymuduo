ff5e4b3990158b08b43043827ccc5dcba75fdacf
xygxgn <hcxygxgn@163.com>
2024-11-18 06:11:29 -0800

    first commit

--- include/Channel.hh
+++ include/Channel.hh
@@ -17,15 +17,47 @@ public:
 
     Channel();
     Channel(EventLoop *loop, int fd);
+
     void handleEvent(Timestamp receieveTime);
 
+    void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
+    void setWriteCallback(EventCallback cb) { writeCallback_ = std::move(cb); }
+    void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
+    void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
+
+    void tie(const std::shared_ptr<void> &);
+
+    int fd() const { return fd_; }
+    int events() const { return events_; }
+    void set_revents(int revents) { revents_ = revents; }
+    bool isNonEvent() const { return events_ == kNoneEvent; }
+
+    void enableReading() { events_ |= kReadEvent; update(); }
+    void disableReading() { events_ &= ~kReadEvent; update(); }
+    void enableWriting() { events_ |= ~kWriteEvent; update(); }
+    void disableWriting() { events_ &= ~kWriteEvent; update(); }
+    void disableAll() { events_ = kNoneEvent; update(); }
+
+    bool isNoneEvent() { return events_ == kNoneEvent; }
+    bool isWriting() { return events_ & kWriteEvent; }
+    bool isReading() { return events_ & kReadEvent; }
+
+    int index() { return index_; }
+    int setIndex(int idx) { index_ = idx; }
+
+    EventLoop* ownerLoop() { return loop_; }
+    void remove();
+
 private:
-    static const int kNodeEvent;
+    void update();
+    void handleEventWithGuard(Timestamp receieveTime);
+
+    static const int kNoneEvent;
     static const int kReadEvent;
     static const int kWriteEvent;
 
     EventLoop *loop_;
-    const int *fd_;
+    const int fd_;
     int events_;
     int revents_;
     int index_;
