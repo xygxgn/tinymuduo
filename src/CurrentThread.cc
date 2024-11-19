155f19011acd758a45bc7b86c04083876eb45dae
xygxgn <hcxygxgn@163.com>
2024-11-19 00:09:07 -0800

    first commit

---
+++ src/CurrentThread.cc
@@ -0,0 +1,17 @@
+#include "CurrentThread.hh"
+
+#include <unistd.h>
+#include <sys/syscall.h>
+
+namespace CurrentThread
+{
+    __thread int t_cacheTid = 0;
+    void cacheTid()
+    {
+        if (t_cachedTid == 0)
+        {
+            t_cachedTid = static_cast<pid_t> (::syscall(SYS_getpid));
+        }
+    }
+
+}
