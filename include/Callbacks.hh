92c6071a0e6b75abac83eb3425428a399b550fb3
xygxgn <hcxygxgn@163.com>
2024-11-21 05:21:30 -0800

    first commit

--- include/Callbacks.hh
+++ include/Callbacks.hh
@@ -13,5 +13,6 @@ using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
 using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
 using WriteCompleteCallback = std::function<void(const TcpConnectionPtr&)>;
 using MessageCallback = std::function<void(const TcpConnectionPtr&, Buffer*, Timestamp)>;
+using HighWaterMarkCallback = std::function<void(const TcpConnectionPtr&, size_t)>;
 
 #endif
--- include/TcpConnection.hh
+++ include/TcpConnection.hh
@@ -1,12 +1,58 @@
 #ifndef _TINYMUDUO_TCPCONNECTION_H
 #define _TINYMUDUO_TCPCONNECTION_H
 
-class TcpConnection
+#include "noncopyable.hh"
+#include "InetAddress.hh"
+#include "Callbacks.hh"
+#include "Buffer.hh"
+
+#include <memory>
+#include <atomic>
+
+class EventLoop;
+class Channel;
+class Socket;
+
+class TcpConnection : noncopyable, public std::enable_shared_from_this<TcpConnection>
 {
 public:
-    TcpConnection();
+    TcpConnection(EventLoop *loop, const std::string &name, int sockfd, const InetAddress &localAddr, const InetAddress &peerAddr);
     ~TcpConnection();
+
+    EventLoop* getLoop() const { return loop_; }
+    const std::string& name() const { return name_;}
+    const InetAddress& localAddress() const { return localAddr_; }
+    const InetAddress& peerAddress() const { return peerAddr_; }
+
+    bool connected() const { return state_ == kConnected; }
 private:
+    enum StateE
+    {
+        kDisconnected,
+        kConnecting,
+        kConnected,
+        kDisconnecting
+    };
+    EventLoop *loop_;
+    const std::string name_;
+    std::atomic<int> state_;
+    bool reading_;
+    std::unique_ptr<Socket> socked_;
+    std::unique_ptr<Channel> channel_;
+
+    const InetAddress localAddr_;
+    const InetAddress peerAddr_;
+
+    ConnectionCallback connectionCallback_;
+    MessageCallback messageCallback_;
+    WriteCompleteCallback writeCompleteCallback_;
+    HighWaterMarkCallback highWaterMarkCallback_;
+    CloseCallback closeCallback_;
+
+    size_t highWaterMark_;
+
+    Buffer inputBuffer_;
+    Buffer outputBuffer_;
 };
 
 #endif
